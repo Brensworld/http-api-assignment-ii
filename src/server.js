ffdfcb3f5bd1e0289cceb5a203067388f2f38288
Brensworld <brensworld83@gmail.com>
2025-09-26 20:40:36 -0400

    added functionality for adding and updating users, and getting users with GET requests

---
+++ src/server.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const query = require('querystring');
+const htmlHandler = require('./htmlHandler.js');
+const jsonHandler = require('./jsonHandler.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const parseBody = (request, response) => {
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    request.body = query.parse(bodyString);
+
+    jsonHandler.addUser(request, response);
+  });
+};
+
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  '/getUsers': jsonHandler.getUsers,
+  '/notReal': jsonHandler.notReal,
+  '/addUser': parseBody,
+  notFound: jsonHandler.notFound,
+};
+
+const onRequest = (request, response) => {
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+
+  if (urlStruct[parsedUrl.pathname]) {
+    return urlStruct[parsedUrl.pathname](request, response);
+  }
+  return urlStruct.notFound(request, response);
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1: ${port}`);
+});
